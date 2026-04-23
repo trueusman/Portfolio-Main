bd4cf8036f15566e9cc88bbd82d10050d6efbc11
Muhammad Usman <truemusman@gmail.com>
2026-04-23 20:21:21 +0500

    update profile img

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I'm Muhammad Usman GHani</span> ,I am a dedicated web developer with a strong interest in building modern, user-friendly digital solutions. Alongside my academic studies, I work on real-world projects to gain hands-on experience in full-stack web development and strengthen my technical foundation.I focus on developing responsive interfaces, clean code, and reliable functionality to deliver effective and well-structured web applications. I am a quick learner, detail-oriented, and committed to continuous growth in the field of web development."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
